3b576adaf1030c6ed75052c191a078f90219b311
kundan <kundan.karna1994@gmail.com>
2024-03-04 10:20:10 +0545

    added toAd function converts bs date to ad date and return carbon date

--- tests/Unit/NepaliDateConverterTest.php
+++ tests/Unit/NepaliDateConverterTest.php
@@ -25,3 +25,8 @@ test("cannot_get_invalid_date", function () {
     $nepaliDate = Carbon::now()->addYears(100)->toBs();
     expect($nepaliDate)->toBeNull();
 });
+
+test("can_get_ad_carbon_date_from_nepali_date", function () {
+    $date = \Proshore\NepaliDate\Facades\NepaliDateConverter::toAd(2080, 01, 06);
+    expect($date)->toBeInstanceOf(Carbon::class);
+});
